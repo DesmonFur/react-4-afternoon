ea875acdddcb4d88fa9f91ca97bd803730945798
DesmondFur <desfurtick@gmail.com>
2019-07-23 16:01:43 -0600

    fin

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import {Link, Switch, Route} from 'react-router-dom'
+import  History from '../History/History'
+import Contact from '../Contact/Contact'
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+          <Link to='/about' className='subnav_links'>
+            <h3>About</h3>
+          </Link>
+          <Link to='/about/history' className='subnav_links'>
+            <h3>History</h3>
+          </Link>
+          <Link to='/about/contact' className='subnav_links'>
+            <h3>Contact</h3>
+          </Link>
         </div>
         <div className='box'>
+          <Switch>
+            <Route component={History} path='/about/history' />
+            <Route component={Contact} path='/about/contact' />
+            <Route
+              path="/about"
+              render={() => (
+                <div>
+                  <h1>About the University</h1>
+                  <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt similique natus hic? Perferendis est, fugit enim laudantium repellendus et sit laboriosam, impedit cum corporis vel excepturi sequi quaerat, praesentium suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, perferendis quidem qui molestias pariatur sed laudantium impedit exercitationem, obcaecati ab ipsam odit nemo cum adipisci eaque dolore unde ipsum! Ab. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab itaque nesciunt sed, veniam et laborum odio quibusdam harum minima nobis aut consequatur? Voluptatibus, dolore perspiciatis. Iste sapiente earum quibusdam.
+                  </p>
+                </div>
+              )}
+            />
+          </Switch>
         </div>
       </div>
     )
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 export default class Student extends Component {
   constructor() {
     super()
-
+    this.state = {
+      studentInfo:{}
+    }
   }
 
-  render() {
-    return (
-      <div className="box">
+
+  componentDidMount(){
+    return axios
+    .get(`http://localhost:3005/students/${this.props.match.params.id}`).then(res => {
+      this.setState({studentInfo: res.data})
+    })
+    }
+
+    
+    render() {
+      const {studentInfo} = this.state
+      return (
+        <div className="box">
         <h1>Student</h1>
+        <h2>{studentInfo.first_name} {studentInfo.last_name}</h2>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <h3>Email: {studentInfo.email}</h3>
       </div>
     )
   }
